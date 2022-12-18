bbd141de9a51490e40bef188ceec07af702bc185
✨CHAIMA✨ <91737328+CHAIMAABDELAZIZ@users.noreply.github.com>
2022-12-18 18:12:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,23 +2,34 @@
 #include "MachineAbstraite.h"
 #include "fonctions.h"
 #include <time.h>
+#include <string.h>
 
-
+char  **nomWilaya,**nomSpecialite, **nomEU ; // tableau pour recuperer les differents noms
 
 int main() {
-    srand(time(aNULL));
-    Tenreg E;
-    BUFFER buf;
-    tBlocIndex buff;
-    L7OF ENS ;
-    indexPrimaire indexPrim ;
-    ENS.indexPr = &indexPrim;
-    chargementInitial(&ENS, "Fichier.bin", 10);
-    Affichage(&ENS, "Fichier.bin");
-    sauvIndexPr("index.bin", &ENS);
-    fichierIndex fich;
-    fich.f = fopen("index.bin","rb");
-    fgets(&buff,10,fich.f);
-    printf("%s",&buff);
+    int c;
+    nomWilaya = fichVersTab("..\\Fichiers_System\\Wilaya.txt", 30, 57, &c);
+    nomSpecialite = fichVersTab("..\\Fichiers_System\\oklm.txt", 30, 11, &c);
+    for (int p = 0; p < 12; ++p) {
+        printf("%s",nomSpecialite[p]);
+    }
+    for (int p = 0; p < 12; ++p) {
+        printf("%s",nomWilaya[p]);
+    }
+    //nomEU = fichVersTab("..\\Fichiers_System\\Eablissement_univ.txt", 70, 111, &c);
+
+    srand(time(NULL));
+        L7OF ENS ;
+        indexPrimaire indexPrim ;
+        ENS.indexPr = &indexPrim;
+        BUFFER buff ;
+
+        chargementInitial(&ENS, "Fichier.bin", 10);
+        Affichage(&ENS, "Fichier.bin");
+        /*sauvIndexPr("index.bin", &ENS);
+        fichierIndex fich;
+        fich.f = fopen("index.bin","rb");
+        fgets(&buff,10,fich.f);
+        printf("%s",&buff);*/
 
 }
