75ac2c8bbd5aa317d26efdd186063805f5c0c805
✨CHAIMA✨ <91737328+CHAIMAABDELAZIZ@users.noreply.github.com>
2022-12-18 18:16:17 +0100

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -4,8 +4,9 @@
 #include <ctype.h>
 #include "MachineAbstraite.h"
 #include "fonctions.h"
-
-
+extern char **nomWilaya;
+extern char **nomSpecialite;
+extern char **nomEU;
 void gen_Matricule (int *matricule) {
     *matricule=0;
     *matricule=(rand()*100)%888888+111111;
@@ -49,10 +50,9 @@ void gen_Date (Date *date) {
 
 }
 
-void gen_Wilaya (char Wilaya[3]) {
+void gen_Wilaya (char Wilaya[30]) {
 
-    sprintf(Wilaya,"%d",rand()%58+1);
-
+    strcpy(Wilaya,nomWilaya[(rand()%58)]);
 
 }
 
@@ -74,12 +74,13 @@ void gen_Dernier_Diplome (char Dernier_Diplome[30]) {
     char tabDD[6][30] = {"Doctorat d'Etat","Doctorat en sciences","Doctorat D_LMD","PhD","Habiliation Universitaire","Magistere"};
     strcpy(Dernier_Diplome,tabDD[rand()%6]);
 }
-void gen_Specialite (char Specialite[4]) {
-    sprintf(Specialite,"%d",rand()%122+1);
+void gen_Specialite (char Specialite[30]) {
+    strcpy(Specialite,nomSpecialite[(rand()%12)]);
 }
 
-void gen_EU (char Etab_Univ[4]) {
-    sprintf(Etab_Univ,"%d",rand()%111+1);
+
+void gen_EU (char Etab_Univ[70]) {
+    sprintf(Etab_Univ,"%d",nomEU[(rand()%111)]);
 }
 
 void gen_GS (char groupeSanguin[4]) {
@@ -100,7 +101,7 @@ Tenreg enseignant() {
     gen_Grade(e.Grade);
     gen_Specialite(e.Specialite);
     gen_Dernier_Diplome(e.Dernier_Diplome);
-    gen_EU(e.Etablissement_Universitaire);
+    //gen_EU(e.Etablissement_Universitaire);
     return e;
 }
 
@@ -122,6 +123,7 @@ void chargementInitial(L7OF *fich, char *nomf, int N) {
         int trouv=1;
         while (trouv)
         {
+
             Ens = enseignant();
             RechDicho(Ens.Matricule, fich->indexPr->tableau, k, &trouv, &pos);
         }
@@ -256,37 +258,50 @@ void insertion_Ens(L7OF *fich, char *nomf) { //Insertion sans table d'index
     fermer(fich);
 }
 
-void suppression_Ens(L7OF *f, char *nomf) {
-    BUFFER buf;
+/*void suppression_Ens(L7OF *f, char *nomf) {
+    BUFFER buf1,buf2;
+    char trouv ;
     int i=2,
-            j=1;
-    Tenreg X;
+    j=1,
+    matricule=0,
+    pos;
+    caseIndexPrimaire X;
     ouvrir(f,nomf,'A');
-    lireDir(f, entete(f,2),&buf);
-    if ((entete(f,2) == entete(f,1)) && buf.NB==1) {
+    RechDicho(matricule, f->indexPr->tableau, f->indexPr->Nenreg, &trouv, &pos);
+    i = f->indexPr->tableau[pos].Nbloc; // bloc ou il se trouve
+    j = f->indexPr->tableau[pos].Deplacement;  // dep  dans le bloc
+    lireDir(f, entete(f,2),&buf1);
+    lireDir(f, i,&buf2);
+
+
+    if ((entete(f,2) == entete(f,1)) && buf1.NB==1) {
         affEntete(f,1,-1);
         affEntete(f,2,-1);
         affEntete(f,3,0);
-        buf.NB=0;
+        buf1.NB=0;
     } else {
 
-        X=buf.tab[buf.NB-1];
-        buf.NB--;
-        if (buf.NB==0) {
+        buf2.tab[j] = buf1.tab[buf1.NB-1];
+        buf1.NB--;
+        if (buf1.NB==0) {
 
             affEntete(f,2, entete(f,2)-1);
-            lireDir(f, entete(f,2),&buf);
-            buf.suivant=-1;
+            lireDir(f, entete(f,2),&buf1);
+            buf1.suivant=-1;
         }
-        ecrireDir(f, entete(f,2),&buf);
-        lireDir(f,i,&buf);
-        buf.tab[j]=X;
+        ecrireDir(f, entete(f,2),&buf1);
+        lireDir(f,i,&buf1);
+        buf1.tab[1] = buf2.tab[j];
         affEntete(f,3, entete(f,3)-1);
 
     }
-    ecrireDir(f, i,&buf);
+    ecrireDir(f, i,&buf1);
+    ecrireDir(f, i,&buf2);
+    //suppression dans la table d'index
+    suppTabOrd(f->indexPr->tableau, f->indexPr->Nenreg, pos);
     fermer(f);
-}
+}*/
+
 
 void Affichage(L7OF *fichier, char *nomf) {
     BUFFER buf;
@@ -294,19 +309,20 @@ void Affichage(L7OF *fichier, char *nomf) {
     int i = 1;
     while (i!=-1) {
         lireDir(fichier, i,&buf);
+
         for (int k = 0; k < buf.NB; ++k) {
             printf(" -%d:%d %d\n",i,k, buf.tab[k].Matricule); //si tu veux tester les autres champs enleve juste les (/**/)
-            /*
-            printf("%s\n",buf.tab[k].Nom);
-            printf("%s\n",buf.tab[k].Prenom);
-            printf("%s\n",buf.tab[k].Sexe);
-            printf("%s/%s/%s\n",buf.tab[k].Date_Naissance.jour,buf.tab[k].Date_Naissance.mois,buf.tab[k].Date_Naissance.annee);
-            printf("%s\n",buf.tab[k].Wilaya);
-            printf("%s\n",buf.tab[k].Groupe_Sanguin);
-            printf("%s\n",buf.tab[k].Grade);
-            printf("%s\n",buf.tab[k].Dernier_Diplome);
-            printf("%s/%s/%s\n",buf.tab[k].Date_Recrutement.jour,buf.tab[k].Date_Recrutement.mois,buf.tab[k].Date_Recrutement.annee);
-        */
+              printf("%s\n",buf.tab[k].Wilaya);
+              printf("%s\n",buf.tab[k].Nom);
+              printf("%s\n",buf.tab[k].Prenom);
+              printf("%s\n",buf.tab[k].Sexe);
+              printf("%s/%s/%s\n",buf.tab[k].Date_Naissance.jour,buf.tab[k].Date_Naissance.mois,buf.tab[k].Date_Naissance.annee);
+              printf("%s\n",buf.tab[k].Groupe_Sanguin);
+              printf("%s\n",buf.tab[k].Grade);
+              printf("%s\n",buf.tab[k].Specialite);
+              printf("%s\n",buf.tab[k].Dernier_Diplome);
+             // printf("%s\n",buf.tab[k].Etablissement_Universitaire);
+              printf("%s/%s/%s\n",buf.tab[k].Date_Recrutement.jour,buf.tab[k].Date_Recrutement.mois,buf.tab[k].Date_Recrutement.annee);
         }
         i=buf.suivant;
     }
@@ -349,7 +365,7 @@ void suppTabOrd(caseIndexPrimaire *tab, int taille, int pos)
 }
 
 void Modification_Ens(char *nomf, L7OF *fich, int matricule) {
-    int trouv,pos,i,j;
+    int trouv,pos,i,j,N,l;
     Tenreg E;
     BUFFER buf;
     RechDicho(matricule, fich->indexPr->tableau, fich->indexPr->Nenreg, &trouv, &pos);
@@ -357,8 +373,13 @@ void Modification_Ens(char *nomf, L7OF *fich, int matricule) {
         i=fich->indexPr->tableau[pos].Nbloc;
         j=fich->indexPr->tableau[pos].Deplacement;
         lireDir(fich,i,&buf);
-
-        strcpy( buf.tab[j].Etablissement_Universitaire,"merde");
+        printf("Veuillez choisir le numero du nouveau etablissement de l'enseignant");
+        for ( l = 0; l <= 111; l++)
+        {
+            printf ("\n %d",nomEU[l]);
+        }
+        scanf("%d",N);
+        strcpy( buf.tab[j].Etablissement_Universitaire,nomEU[l]);
     }
 }
 
@@ -403,4 +424,47 @@ void sauvIndexPr(char nomIndex[], L7OF *fichier)
         fclose(ifichier.f);
     }
 }
+
+
+//  retourne un mot avec seulement des caractere alphabetique
+char *motPure(char *mot)
+{
+
+    char delimiteurs[] = "/\\\t\n;:.=*\"(){}<>!?&$1234567890"; //caratere non alphabetique
+    return strtok(mot, delimiteurs);
+}
+
+
+//  transform un fichier texte utilse comme une base de donnes vers un tableau de nbMot case chaque est une chaine de taille taiMot , cpt est le nombre des cases remplis dans le tableau
+char **fichVersTab(char nomf[], int taiMot, int nbMot, int *cpt)
+{
+
+    FILE *f = fopen(nomf, "r");
+    char mot[taiMot];
+    char **tabMot = (char **)malloc(sizeof(char *) * nbMot); //alocation dynamique du tableau
+    *cpt = 0;
+
+    if (f != NULL)
+
+        while (!feof(f))
+        {    {
+
+                fgets(mot, 100, f); //recuperer la ligne
+            if (motPure(mot) != NULL) //s'il existe un mot dans la ligne
+            {
+                strcpy(mot, motPure(mot)); // recuperer le motPure
+                tabMot[*cpt] = (char *)malloc(sizeof(char) * taiMot);
+                strcpy(tabMot[*cpt], mot); //copier le mot dans une case dans tabMot
+                *cpt = *cpt + 1;
+            }
+        }
+    }
+    for (int p = 0; p < 12; ++p) {
+        printf("%s",tabMot[p]);
+    }
+    fclose(f);
+    return tabMot;
+}
+
+
 
