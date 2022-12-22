a173bc71d5f387d8996b2ad572472532ba4b8de8
✨CHAIMA✨ <91737328+CHAIMAABDELAZIZ@users.noreply.github.com>
2022-12-22 02:16:00 +0100

    Update machineabstraite.h

--- machineabstraite.h
+++ machineabstraite.h
@@ -5,7 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 9999
-#define b 10
+#define b 100
+#define NB_Specialite 12
+#define NB_EU 113
+
 
 
 
@@ -28,7 +31,7 @@ typedef struct Tenreg
     char Groupe_Sanguin[4];
     Date Date_Recrutement;
     char Grade[30];
-    char Specialite[30];
+    char Specialite[35];
     char Dernier_Diplome[30];
     char Etablissement_Universitaire[97];
 }Tenreg;
@@ -71,18 +74,32 @@ typedef struct fichierIndex
     indexEntete entete;
 } fichierIndex;
 
-typedef struct tBolcIndex
+typedef struct tBlocIndex
 {
     caseIndexPrimaire tab[b];
     int NB;
 
 } tBlocIndex;
 
+typedef struct maillon
+{
+    int matricule;
+    struct mallion *suivant;
+
+} maillon;
+
+typedef struct caseSec {
+    maillon *info;
+    int nbmat;
+}caseSec;
+
 typedef struct L7OF
 {
     FILE *f;
     Entete *entete;
     indexPrimaire *indexPr;
+    caseSec iSpecialite[NB_Specialite]; //indexSec pour la spécialité
+    caseSec iEU[NB_EU];
 
 
 } L7OF;
