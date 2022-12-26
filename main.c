04560fb46041c0d166e9d012cdfd4522720678b4
touilnihel <93738618+touilnihel@users.noreply.github.com>
2022-12-27 11:21:19 +1300

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 
 char  **nomWilaya,**nomSpecialite, **nomEU, **region_centre, **region_est, **region_ouest ; // tableau pour recuperer les differents noms
 
+int nbLireDir=0,
+nbEcrireDir=0;
+
 int main() {
     int c,v1,v2;
     nomWilaya = fichVersTab("..\\Fichiers_System\\Wilaya.txt", 22, 58, &c);
